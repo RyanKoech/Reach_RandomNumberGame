c4d8b6109c1ef97891a8374ea49dd47b61e1b61c
RyanKoech <sirryankoech@gmail.com>
2022-08-08 08:40:05 +0300

    Updated constants

--- other/Constants.js
+++ other/Constants.js
@@ -1,4 +1,7 @@
-export const WRAPPER = "Wrapper";
-export const CONNECT_WALLET = "ConnectWallet";
-export const FUND_ACCOUNT = "FundAccount";
+export const AWAIT_RESULTS = "AwaitResults";
 export const CHOOSE_ROLE = "ChooseRole";
+export const CONNECT_WALLET = "ConnectWallet";
+export const DONE = "Done";
+export const FUND_ACCOUNT = "FundAccount";
+export const TIMEOUT = "Timeout";
+export const WRAPPER = "Wrapper";
