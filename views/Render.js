c2510401c518a8d2b27bf17b3bf718664e1e9ffe
RyanKoech <sirryankoech@gmail.com>
2022-08-08 08:34:01 +0300

    Added custom rendering logic

--- views/Render.js
+++ views/Render.js
@@ -7,3 +7,15 @@ export function renderDOM(app) {
     <React.StrictMode>{app}</React.StrictMode>
   );
 }
+
+export function renderView(parent, Views) {
+  parent.state = parent.state || {};
+  const {view, ContentView} = parent.state;
+  const View = view === 'Wrapper'
+    ? ContentView
+    : Views[view];
+  const Wrapper = Views['Wrapper'];
+  const props = {...parent.props, ...parent.state, parent};
+  const content = <View {...props} />;
+  return <Wrapper {...{content, bal: props.bal, standardUnit : props.standardUnit}} />;
+}
