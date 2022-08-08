cf43dfcf31c13f47f4b914e027934428d34c7edb
RyanKoech <sirryankoech@gmail.com>
2022-08-08 14:50:20 +0300

    Implemented logic into deployer view elements

--- views/DeployerViews.js
+++ views/DeployerViews.js
@@ -17,30 +17,35 @@ DeployerViews.Wrapper = class extends React.Component {
 
 DeployerViews.SetGameTerms = class extends React.Component {
   render() {
+    const {parent, defaultWager, standardUnit} = this.props;
+    const wager = (this.state || {}).wager || defaultWager;
+    const lowerLimit = (this.state || {}).lowerLimit || 10;
+    const upperLimit = (this.state || {}).upperLimit || 20;
     return (
       <div>
+        <label>Wager : </label>
         <input
           type='number'
-          placeholder={'3'}
-          onChange={(e) => {}}
+          placeholder={wager}
+          onChange={(e) => {this.setState({wager: e.currentTarget.value})}}
         /> ALGO
         <br />
         <label>Lower Range Limit : </label>
         <input
           type='number'
-          placeholder={'0'}
-          onChange={(e) => {}}
+          placeholder={lowerLimit}
+          onChange={(e) => {this.setState({lowerLimit: e.currentTarget.value})}}
         />
         <br />
         <label>Upper Range Limit : </label>
         <input
           type='number'
-          placeholder={'10'}
-          onChange={(e) => {}}
+          placeholder={upperLimit}
+          onChange={(e) => {this.setState({upperLimit: e.currentTarget.value})}}
         />
         <br />
         <button
-          onClick={() => {}}
+          onClick={() => {parent.setGameTerms(wager, lowerLimit, upperLimit)}}
         >Set Game Terms</button>
       </div>
     );
@@ -52,10 +57,10 @@ DeployerViews.Deploy = class extends React.Component {
     const {parent, wager, standardUnit} = this.props;
     return (
       <div>
-        Wager (pay to deploy): <strong> 30 </strong> ALGO
+        Wager (pay to deploy): <strong> {wager} </strong> {standardUnit}
         <br />
         <button
-          onClick={() => {}}
+          onClick={() => {parent.deploy()}}
         >Deploy</button>
       </div>
     );
@@ -70,20 +75,33 @@ DeployerViews.Deploying = class extends React.Component {
   }
 }
 
-DeployerViews.WaitingForAttacher = class extends React.Component {
+DeployerViews.AwaitingAttacher = class extends React.Component {
+  async copyToClipboard(button) {
+    const {ctcInfoStr} = this.props;
+    navigator.clipboard.writeText(ctcInfoStr);
+    const origInnerHTML = button.innerHTML;
+    button.innerHTML = 'Copied!';
+    button.disabled = true;
+    await sleep(1000);
+    button.innerHTML = origInnerHTML;
+    button.disabled = false;
+  }
 
   render() {
-    const {ctcInfoStr} = this.props;
+    const {ctcInfoStr, parent} = this.props;
     return (
       <div>
         Waiting for Attacher to join...
         <br /> Please give them this contract info:
-        <pre className='ContractInfo'>
-          Contract info
+        <pre>
+        {ctcInfoStr}
         </pre>
         <button
-          onClick={(e) => {}}
+          onClick={(e) => {this.copyToClipboard(e.currentTarget)}}
         >Copy to clipboard</button>
+        <button
+          onClick={(e) => {parent.startGame();}}
+        >Start Game</button>
       </div>
     )
   }
